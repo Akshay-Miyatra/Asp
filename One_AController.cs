7af7c132550283f39e90afe36b7db1c47557cfa9
Akshay-Miyatra <mrakmiyatra@gmail.com>
2025-08-04 12:45:35 +0530

    Add files via upload

---
+++ One_AController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignamnet_four_q1_Akshay.Controllers
+{
+    public class One_AController : Controller
+    {
+        // GET: One_A
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult test(int? start, int? end)
+        {
+            if (start == null || end == null)
+            {
+                ViewBag.Message = "Please enter valid start and end values.";
+                return View();
+            }
+            ViewBag.st = start.Value;
+            ViewBag.en = end.Value;
+            return View();
+        }
+
+
+    }
+}
